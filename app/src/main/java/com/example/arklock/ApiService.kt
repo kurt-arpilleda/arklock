a67d8451c21b43a0d7772fdb016bc79205202c11
Kurt Bryant <BCArpilleda@gmail.com>
2025-07-12 15:00:44 +0800

    For God so Love the World that He gave His only son that whoever believes in Him will not perish but everlasting Life

--- app/src/main/java/com/example/arklock/ApiService.kt
+++ app/src/main/java/com/example/arklock/ApiService.kt
@@ -1,10 +1,7 @@
 package com.example.arklock
 
 import retrofit2.Call
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
-import retrofit2.http.POST
 
 interface ApiService {
     @GET("V4/Others/Kurt/LatestVersionAPK/ArkLock/output-metadata.json")
