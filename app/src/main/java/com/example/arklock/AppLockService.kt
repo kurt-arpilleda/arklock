05d95cf4d0c2014f833b166b184e186643ea7164
Kurt Bryant <BCArpilleda@gmail.com>
2025-07-18 15:41:47 +0800

    For God so Love the World that He gave His only son that whoever believes in Him will not perish but everlasting Life

--- app/src/main/java/com/example/arklock/AppLockService.kt
+++ app/src/main/java/com/example/arklock/AppLockService.kt
@@ -2,10 +2,8 @@ package com.example.arklock
 
 import android.annotation.SuppressLint
 import android.app.ActivityManager
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.app.Service
 import android.app.usage.UsageStatsManager
 import android.content.BroadcastReceiver
@@ -13,7 +11,6 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.content.SharedPreferences
-import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Handler
 import android.os.IBinder
